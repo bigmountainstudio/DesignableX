5c502afd071bd098681948203c638c503991ad9b
Mark Moeykens <mark.moeykens@gmail.com>
2016-12-31 11:42:50 -0700

    Added UIViewX

---
+++ DesignableX/UIViewX.swift
@@ -0,0 +1,57 @@
+//
+//  UIViewX.swift
+//  DesignableX
+//
+//  Created by Mark Moeykens on 12/31/16.
+//  Copyright © 2016 Mark Moeykens. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class UIViewX: UIView {
+
+    @IBInspectable public var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable public var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable public var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    // MARK: - Shadow
+    
+    @IBInspectable public var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable public var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable public var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable public var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
