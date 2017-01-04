11ae53e96cedd3353d32af5311017228a248e0a7
Mark Moeykens <mark.moeykens@us.usana.com>
2017-01-04 07:48:24 -0700

    Added the beginning of a new designable button. Limited animation right now. Will update later.

---
+++ DesignableX/UIButtonX.swift
@@ -0,0 +1,70 @@
+//
+//  DesignableButton.swift
+//  SkyApp
+//
+//  Created by Mark Moeykens on 12/18/16.
+//  Copyright © 2016 Mark Moeykens. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class UIButtonX: UIButton {
+    
+    enum FromDirection:Int {
+        case Top = 0
+        case Right = 1
+        case Left = 2
+        case Bottom = 3
+    }
+    
+    var direction: FromDirection = .Left
+    
+    override func awakeFromNib() {
+        
+        let originalFrame = frame
+        
+        if direction == .Bottom {
+            frame = CGRect(x: frame.origin.x, y: frame.origin.y + 200, width: frame.width, height: frame.height)
+        }
+        
+        if animate {
+            UIView.animate(withDuration: 0.3, delay: animateDelay, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .allowUserInteraction, animations: {
+                self.frame = originalFrame
+            }, completion: nil)
+        }
+    }
+    
+    @IBInspectable var animate: Bool = false
+    @IBInspectable var animateDelay: Double = 0.2
+    @IBInspectable var fromDirection:Int {
+        get {
+            return direction.rawValue
+        }
+        set (directionIndex) {
+            direction = FromDirection(rawValue: directionIndex) ?? .Left
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        self.clipsToBounds = true
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
