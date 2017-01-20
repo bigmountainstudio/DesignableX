1e93af65d74c25892ece6869f95a9014af1e58e4
Mark Moeykens <mark.moeykens@gmail.com>
2017-01-19 18:29:12 -0700

    Added rightView to UITextFieldX

--- DesignableXTesting/DesignableXTesting/DesignableX/UITextFieldX.swift
+++ DesignableXTesting/DesignableXTesting/DesignableX/UITextFieldX.swift
@@ -23,7 +23,38 @@ class UITextFieldX: UITextField {
         }
     }
     
+    @IBInspectable var rightImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var rightPadding: CGFloat = 0 {
+        didSet {
+            updateView()
+        }
+    }
+    
+    private var _isRightViewVisible: Bool = true
+    var isRightViewVisible: Bool {
+        get {
+            return _isRightViewVisible
+        }
+        set {
+            _isRightViewVisible = newValue
+            updateView()
+        }
+    }
+    
     func updateView() {
+        setLeftImage()
+        setRightImage()
+        
+        // Placeholder text color
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSForegroundColorAttributeName: tintColor])
+    }
+    
+    func setLeftImage() {
         leftViewMode = UITextFieldViewMode.always
         var view: UIView
         
@@ -46,10 +77,35 @@ class UITextFieldX: UITextField {
         }
         
         leftView = view
-        
-        // Placeholder text color
-        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSForegroundColorAttributeName: tintColor])
     }
+    
+    func setRightImage() {
+        rightViewMode = UITextFieldViewMode.always
+        
+        var view: UIView
+        
+        if let image = rightImage, isRightViewVisible {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+            imageView.image = image
+            // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
+            imageView.tintColor = tintColor
+            
+            var width = image.size.width + rightPadding
+            
+            if borderStyle == UITextBorderStyle.none || borderStyle == UITextBorderStyle.line {
+                width += 5
+            }
+            
+            view = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 20))
+            view.addSubview(imageView)
+            
+        } else {
+            view = UIView(frame: CGRect(x: 0, y: 0, width: rightPadding, height: 20))
+        }
+        
+        rightView = view
+    }
+    
     
     // MARK: - Corner Radius
     @IBInspectable var cornerRadius: CGFloat = 0 {
