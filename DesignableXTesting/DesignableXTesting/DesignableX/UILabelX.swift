dbeeedc754eaa50524eff1418b3729e1319c6ca9
Mark Moeykens <mark.moeykens@gmail.com>
2017-01-28 11:22:55 -0700

    Added UILabelX

---
+++ DesignableXTesting/DesignableXTesting/DesignableX/UILabelX.swift
@@ -0,0 +1,31 @@
+//
+//  UILabelX.swift
+//  DesignableXTesting
+//
+//  Created by Mark Moeykens on 1/28/17.
+//  Copyright © 2017 Moeykens. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class UILabelX: UILabel {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
